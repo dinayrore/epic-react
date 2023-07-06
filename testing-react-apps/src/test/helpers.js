294885cfbb1d5fa732ae416bc96de5b3a33b17e4
Kris <horn.kristeen@gmail.com>
2023-07-06 14:26:23 -0400

    - exercise four complete

--- testing-react-apps/src/test/helpers.js
+++ testing-react-apps/src/test/helpers.js
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 export const incrementClickEvent = new MouseEvent('click', {
     bubbles: true,
     cancelable: true,
@@ -9,3 +11,11 @@ export const incrementClickEvent = new MouseEvent('click', {
     cancelable: true,
     button: 0,
   })
+
+  export const generateLoginFormData = (overrides) => {
+    return {
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
+      ...overrides
+    }
+  }
