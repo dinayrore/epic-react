b8b13e5dcd387807ea95390b5fbdd844d44d847a
Kris <horn.kristeen@gmail.com>
2023-07-07 09:58:36 -0400

    - exercises six and seven complete

--- testing-react-apps/src/test/helpers.js
+++ testing-react-apps/src/test/helpers.js
@@ -1,4 +1,6 @@
 import faker from 'faker'
+import { ThemeProvider } from 'components/theme'
+import { render } from '@testing-library/react'
 
 export const incrementClickEvent = new MouseEvent('click', {
     bubbles: true,
@@ -18,4 +20,11 @@ export const incrementClickEvent = new MouseEvent('click', {
       password: faker.internet.password(),
       ...overrides
     }
+  }
+
+export const renderWithProviders = (ui, {theme = 'light', ...options} = {}) => {
+    const Wrapper = ({children}) => (
+      <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+    )
+    return render(ui, {wrapper: Wrapper, ...options})
   }
