bcfc5807707884c16dd0096a9d311f9af81ec459
Kris <horn.kristeen@gmail.com>
2023-07-07 12:03:47 -0400

    -exercise one complete

---
+++ react-suspense/src/helpers.js
@@ -0,0 +1,21 @@
+// 💯 generic createResource
+export const createResource = (promise) => {
+    let status = 'pending'
+    let result = promise.then(
+      resolved => {
+        status = 'success'
+        result = resolved
+      },
+      rejected => {
+        status = 'error'
+        result = rejected
+      },
+    )
+    return {
+      read() {
+        if (status === 'pending') throw result
+        if (status === 'error') throw result
+        if (status === 'success') return result
+      },
+    }
+  }
